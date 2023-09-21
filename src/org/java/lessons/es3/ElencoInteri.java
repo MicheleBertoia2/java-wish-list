606ad52fd59bed5fb50bf47ec86a5c9024cc09b1
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-09-21 15:11:32 +0200

    add bonus

--- src/org/java/lessons/es3/ElencoInteri.java
+++ src/org/java/lessons/es3/ElencoInteri.java
@@ -1,8 +1,13 @@
 package org.java.lessons.es3;
 
 public class ElencoInteri {
-	private int[] elenco;
+	private int[] elenco = new int[0];
 	private int counter = 0;
+	
+	public ElencoInteri()
+	{
+		
+	};
 
 	public ElencoInteri(int[] elenco) {
 		
@@ -37,6 +42,26 @@ public class ElencoInteri {
 	boolean hasAncoraElementi()
 	{
 		return (getCounter() < getElenco().length) ? true : false;
+	}
+	
+	public int length()
+	{
+		return getElenco().length;
+	}
+	
+	public void addElemento(int elem)
+	{
+		int[] oldArray = getElenco();
+		int[] newArray = new int[getElenco().length + 1];
+
+
+		for(int i = 0; i < oldArray.length; i++)
+		{
+			newArray[i] = oldArray[i];
+		}
+		newArray[newArray.length - 1] = elem;
+		
+		setElenco(newArray);
 	}
 	
 	public void reset()
--- src/org/java/lessons/es3/Main.java
+++ src/org/java/lessons/es3/Main.java
@@ -1,5 +1,7 @@
 package org.java.lessons.es3;
 
+import java.util.Random;
+
 public class Main {
 	public static void main(String[] args) {
 		
@@ -11,6 +13,27 @@ public class Main {
 		while(elenco.hasAncoraElementi())
 		{
 			System.out.println(elenco.getElementoSuccessivo());
+		}
+		
+		System.out.println("\n------------------------\n");
+		
+		ElencoInteri elenco2 = new ElencoInteri();
+		Random rnd = new Random();
+		
+		for(int i = 0; i < 5; i++)
+		{
+			elenco2.addElemento(rnd.nextInt(0,51));
+		}
+		
+		elenco2.addElemento(3);
+		elenco2.addElemento(4);
+		elenco2.addElemento(7);
+		
+		
+		
+		while(elenco2.hasAncoraElementi())
+		{
+			System.out.println(elenco2.getElementoSuccessivo());
 		}
 	}
 }
